b7ee1630c9fa05769bcaa36df9c44a3c00f0330b
deatil <2217957370@qq.com>
2021-09-25 23:59:48 +0800

    更新

--- admin/cmd/reset_permission.go
+++ admin/cmd/reset_permission.go
@@ -4,6 +4,11 @@ import (
     "fmt"
 
     "github.com/spf13/cobra"
+
+    "lakego-admin/lakego/facade/casbin"
+    casbinAdapter "lakego-admin/lakego/casbin/adapter"
+
+    "lakego-admin/admin/model"
 )
 
 /**
@@ -25,7 +30,60 @@ var ResetPermissionCmd = &cobra.Command{
 
     },
     Run: func(cmd *cobra.Command, args []string) {
-        fmt.Println("成功！")
+        ResetPermission()
     },
+}
+
+// 重设权限
+func ResetPermission() {
+    // 清空原始数据
+    model.NewDB().Where("1 = 1").Delete(&casbinAdapter.Rules{})
+
+    // 权限
+    ruleList := make([]model.AuthRuleAccess, 0)
+    err := model.NewAuthRuleAccess().
+        Preload("Rule", "status = ?", 1).
+        Find(&ruleList).
+        Error
+    if err != nil {
+        fmt.Println("权限同步失败")
+        return
+    }
+
+    ruleListMap := model.FormatStructToArrayMap(ruleList)
+
+    // 分组
+    groupList := make([]model.AuthGroupAccess, 0)
+    err2 := model.NewAuthGroupAccess().
+        Preload("Group", "status = ?", 1).
+        Find(&groupList).
+        Error
+    if err2 != nil {
+        fmt.Println("权限同步失败")
+        return
+    }
+
+    groupListMap := model.FormatStructToArrayMap(groupList)
+
+    // casbin
+    cas := casbin.New()
+
+    // 添加权限
+    if len(ruleListMap) > 0 {
+        for _, rv := range ruleListMap {
+            rule := rv["Rule"].(map[string]interface{})
+
+            cas.AddPolicy(rv["group_id"].(string), rule["auth_url"].(string), rule["method"].(string))
+        }
+    }
+
+    // 添加权限
+    if len(groupListMap) > 0 {
+        for _, gv := range groupListMap {
+            cas.AddRoleForUser(gv["admin_id"].(string), gv["group_id"].(string))
+        }
+    }
+
+    fmt.Println("权限同步成功")
 }
 
