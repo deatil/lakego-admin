343398aa6892fae2e79ddd4c02f4d3dcaa0ecd61
deatil <2217957370@qq.com>
2021-10-29 23:57:13 +0800

    更新

--- pkg/lakego-admin/lakego/http/request/interface.go
+++ pkg/lakego-admin/lakego/http/request/interface.go
@@ -1,22 +1,27 @@
 package request
 
+// json 输出
 type JSONWriter interface {
     JSON(code int, data interface{})
 }
 
+// 查询读
 type QueryReader interface {
     Query(key string) string
     DefaultQuery(key string, def string) string
 }
 
+// 路径数据读
 type PathParamReader interface {
     Param(key string) string
 }
 
+// 写接口
 type Writer interface {
     JSONWriter
 }
 
+// 读接口
 type Reader interface {
     BindJSON(i interface{}) error
     ShouldBind(i interface{}) error
