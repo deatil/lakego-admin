60dd502a7b010f11574f1d12237bab1acf0e8df8
deatil <2217957370@qq.com>
2025-06-06 12:41:44 +0800

    更新

--- pkg/lakego-pkg/go-cryptobin/cryptobin/crypto/encrypt/salsa20/salsa20_test.go
+++ pkg/lakego-pkg/go-cryptobin/cryptobin/crypto/encrypt/salsa20/salsa20_test.go
@@ -20,7 +20,7 @@ func Test_Salsa20(t *testing.T) {
     data := "test-pass"
     cypt := crypto.FromString(data).
         SetKey("1234567890abcdef1234567890abcdef").
-        SetIv("1234567890abcdef").
+        SetIv("12abcdef").
         MultipleBy(Salsa20).
         Encrypt()
     cyptStr := cypt.ToBase64String()
@@ -29,7 +29,7 @@ func Test_Salsa20(t *testing.T) {
 
     cyptde := crypto.FromBase64String(cyptStr).
         SetKey("1234567890abcdef1234567890abcdef").
-        SetIv("1234567890abcdef").
+        SetIv("12abcdef").
         MultipleBy(Salsa20).
         Decrypt()
     cyptdeStr := cyptde.ToString()
